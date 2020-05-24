86d5c8391194f09023f80f00b63a0537ef5d6472
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-24 11:56:15 -0500

    replace the container widget with a column widget and in it's children add three expanded widgets. In those three expanded widgets add rows accordingly.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -8,21 +9,71 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body:Container(
-        margin:EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color:Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
-
-
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
         ),
-        height:200,
-        width: 170,
-      )
+        body: Column(
+          children: <Widget>[
+            Expanded(child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                  margin:EdgeInsets.all(15),
+                  decoration: BoxDecoration(  color:Color(0xFF1DE33),
+                  borderRadius: BorderRadius.circular(10),)
 
+
+                  ),
+                ),
+
+                Expanded(
+                  child: Container(
+                      margin:EdgeInsets.all(15),
+                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
+                        borderRadius: BorderRadius.circular(10),)
+
+
+                  ),
+                ),
+
+
+              ],
+            )),
+            Expanded(child: Container(
+                margin:EdgeInsets.all(15),
+                decoration: BoxDecoration(  color:Color(0xFF1DE33),
+                  borderRadius: BorderRadius.circular(10),)
+
+
+            ),),
+            Expanded(child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                      margin:EdgeInsets.all(15),
+                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
+                        borderRadius: BorderRadius.circular(10),)
+
+
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      margin:EdgeInsets.all(15),
+                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
+                        borderRadius: BorderRadius.circular(10),)
+
+
+                  ),
+                ),
+
+
+
+              ],
+            )),
+          ]
+
+        )
     );
   }
 }
