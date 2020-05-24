f1c3fd168a49c81bd1e536c4babc45a9e8e70453
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-24 12:13:09 -0500

    replace the occurance of container widget with ReusableCard() where ever it is used

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -25,31 +25,16 @@ class _InputPageState extends State<InputPage> {
             ],
           )),
           Expanded(
-            child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1DE33),
-                  borderRadius: BorderRadius.circular(10),
-                )),
+            child:  ReusableCard(),
           ),
           Expanded(
               child: Row(
             children: <Widget>[
               Expanded(
-                child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1DE33),
-                      borderRadius: BorderRadius.circular(10),
-                    )),
+                child: ReusableCard(),
               ),
               Expanded(
-                child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1DE33),
-                      borderRadius: BorderRadius.circular(10),
-                    )),
+                child:  ReusableCard(),
               ),
             ],
           )),
