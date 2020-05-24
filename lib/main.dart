b546ea3b6235fa72743a2e0f1fad8543aab9c484
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-24 11:16:01 -0500

    use existing dark() theme and along with it use copywith() method to change certain things you want to

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
-        accentColor: Colors.purple,
+      theme: ThemeData.dark().copyWith(
+            primaryColor: Color(0xFF0A0E21),
+            scaffoldBackgroundColor: Color(0xFF0A0E21),
+            accentColor: Colors.purple,
 
-      ),
+        ),
+
       home: InputPage(),
     );
   }
