86213cd5d658f01065801e39ae1d173cb9ce26d5
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-24 12:11:39 -0500

    extract the container widget in flutter outline and the container widget will be replaced by the refracted name.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,68 +12,63 @@ class _InputPageState extends State<InputPage> {
         appBar: AppBar(
           title: Text('BMI CALCULATOR'),
         ),
-        body: Column(
-          children: <Widget>[
-            Expanded(child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                  margin:EdgeInsets.all(15),
-                  decoration: BoxDecoration(  color:Color(0xFF1DE33),
-                  borderRadius: BorderRadius.circular(10),)
+        body: Column(children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
+            ],
+          )),
+          Expanded(
+            child: Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1DE33),
+                  borderRadius: BorderRadius.circular(10),
+                )),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                    margin: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1DE33),
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              Expanded(
+                child: Container(
+                    margin: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1DE33),
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+            ],
+          )),
+        ]));
+  }
+}
 
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
 
-                  ),
-                ),
-
-                Expanded(
-                  child: Container(
-                      margin:EdgeInsets.all(15),
-                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
-                        borderRadius: BorderRadius.circular(10),)
-
-
-                  ),
-                ),
-
-
-              ],
-            )),
-            Expanded(child: Container(
-                margin:EdgeInsets.all(15),
-                decoration: BoxDecoration(  color:Color(0xFF1DE33),
-                  borderRadius: BorderRadius.circular(10),)
-
-
-            ),),
-            Expanded(child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                      margin:EdgeInsets.all(15),
-                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
-                        borderRadius: BorderRadius.circular(10),)
-
-
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                      margin:EdgeInsets.all(15),
-                      decoration: BoxDecoration(  color:Color(0xFF1DE33),
-                        borderRadius: BorderRadius.circular(10),)
-
-
-                  ),
-                ),
-
-
-
-              ],
-            )),
-          ]
-
-        )
-    );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Color(0xFF1DE33),
+          borderRadius: BorderRadius.circular(10),
+        ));
   }
 }
