0614a683c02d8a47db6a3e6351cc42e185d8d720
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-28 14:09:44 -0500

    add min and max values in slider.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -89,6 +89,8 @@ class _InputPageState extends State<InputPage> {
                   ),
                   Slider(
                     value: height.toDouble(),
+                      min:120.0,
+                    max: 220.0,
 
 
                   ),
