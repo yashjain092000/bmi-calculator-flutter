d426ba69d7bcf2a29f037f4c9fee6eb9f8804daa
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-05-29 22:58:38 -0500

    create a method CalculateBMI that returns the String of calculated bmi.

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,5 +1,13 @@
-class CalculatorBrain{
-  CalculatorBrain(this.height,this.weight);
-final int height;
-final int weight;
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain(this.height, this.weight);
+
+  final int height;
+  final int weight;
+
+  String CalculateBMI() {
+    double bmi = weight / pow(height/100, 2);
+    return bmi.toStringAsFixed(1);
+  }
 }
